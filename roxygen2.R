a178642440b6c14b2e2204743a499fc653507060
Guido Schwarzer <sc@imbi.uni-freiburg.de>
2019-01-09 19:49:08 +0100

    Use RStudio for package development

---
+++ roxygen2.R
@@ -0,0 +1,36 @@
+##
+## (1) Make R packages available
+##
+library(devtools)
+library(roxygen2)
+
+
+##
+## (2) Create documentation file(s) in subdirectory testroxygen/man
+##
+document("../metasens") # Also considers datasets in subdirectory metasens/data
+
+
+##
+## (3) Build R package and PDF file with help pages
+##
+build("../metasens")
+build_manual("../metasens")
+
+
+##
+## (4) Install R package
+##
+install("../metasens")
+
+
+##
+## (5) Check R package
+##
+check("../metasens")
+
+
+##
+## (6) Check R package (with dontrun examples)
+##
+check("../metasens", run_dont_test = TRUE)
