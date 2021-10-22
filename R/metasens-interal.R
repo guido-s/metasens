e38294bc4235f9fceea57753a012413016658bcc
Guido Schwarzer <sc@imbi.uni-freiburg.de>
2021-10-22 20:09:35 +0200

    Update start-up message

--- R/metasens-interal.R
+++ R/metasens-interal.R
@@ -1,6 +1,10 @@
 .onAttach <- function(libname, pkgname) {
-  msg <- paste("Loading 'metasens' package (version ",
-               utils::packageDescription("metasens")$Version,
-               ").", sep = "")
+  msg <-
+    paste0("Loading 'metasens' package (version ",
+           utils::packageDescription("metasens")$Version,
+           ").",
+           "\nReaders of 'Meta-Analysis with R (Use R!)' should install",
+           "\nolder version of 'metasens' package: ",
+           "https://tinyurl.com/jhv33bfm")           
   packageStartupMessage(msg)
 }
